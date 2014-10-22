15d8424acdc4ba311b8ed820a8b5f0f69e85072c
teddy <teddy@teddydeiMac.local>
2014-10-22 09:26:46 +0800

    Initial Commit

---
+++ ReserviorStandard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ReserviorStandard
+//
+//  Created by teddy on 14-10-22.
+//  Copyright (c) 2014年 teddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ReserviorStandard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ReserviorStandard
+//
+//  Created by teddy on 14-10-22.
+//  Copyright (c) 2014年 teddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
